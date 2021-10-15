2e1e5525ab6c6dba0dbd41731cc7f3953888435b
Amelia Denison <millyd_12@hotmail.com>
2021-10-14 21:29:50 -0600

    Added comment as a greeting.

--- src/meusebio/HelloFromMarlon.java
+++ src/meusebio/HelloFromMarlon.java
@@ -5,5 +5,6 @@ public class HelloFromMarlon {
     public static void main(String[] args) {
         // Print out the Hello World
         System.out.println("Hello, my name is Marlon Eusebio.");
+        //Hi, Marlon. I finally figured out how to do this :D
     }
 }
